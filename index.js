bd631156ea829b011929c5835d1f1b029b925c91
Tom Golebiewski <tgolebie16@gmail.com>
2023-08-12 03:20:25 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const messages = names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
