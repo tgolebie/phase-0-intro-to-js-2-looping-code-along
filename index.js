d42c6861fcb4e145673c5af703d3770ccff7a47b
Tom Golebiewski <tgolebie16@gmail.com>
2023-08-12 07:44:52 -0400

    lab done

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-function writeCards(names, occasion) {
-    const messages = names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
-    return messages;
+function writeCards(names) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    thankYouMessages.push(message);
   }
-  
-  function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-      console.log(i);
-    }
+
+  return thankYouMessages;
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = writeCards(names);
+
+console.log(messages);
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('countDown()', () => {
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+
+  it('invokes console.log once for each number, counting down from the number provided to zero', () => {
+    countDown(10);
+    expect(spy.callCount, "Expected countDown(10) to invoke 11 console.logs").to.eq(11);
+  });
+
+  it('logs each number when counting down, starting from the number provided', () => {
+    countDown(4);
+    expect(spy.calledWithExactly(4), "Expected countDown(4) to log 4 first").to.be.true;
+    expect(spy.calledWithExactly(3), "Expected countDown(4) to log 3 after 4").to.be.true;
+    expect(spy.calledWithExactly(2), "Expected countDown(4) to log 2 after 3").to.be.true;
+    expect(spy.calledWithExactly(1), "Expected countDown(4) to log 1 after 2").to.be.true;
+    expect(spy.calledWithExactly(0), "Expected countDown(4) to log 0 after 1").to.be.true;
+  });
+});
+
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
   }
-  
-  module.exports = {
-    writeCards,
-    countDown
+}
